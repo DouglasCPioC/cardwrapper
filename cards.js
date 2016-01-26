9a0d7c36a7692bb574ad4d247bb3c22c22a41c69
Douglas Cesar Pio do Carmo <doerdeiro@gmail.com>
2016-01-26 06:39:52 -0200

    Criacao dinamica de div (card) no meio da div maior.

--- cards.js
+++ cards.js
@@ -1,5 +1,12 @@
-function cardCreator() {
+var cardCounter = 0;
+var cardCounterOld = 0
+function cardCreator() { 
 	var iDiv = document.createElement('div');
 	iDiv.className = 'card';
+	iDiv.id = 'card-' + cardCounter;
 	document.getElementsByClassName('card-wrapper')[0].appendChild(iDiv);
+	document.getElementById('card-' + cardCounterOld).style.left = '50%';
+	document.getElementById('card-' + cardCounterOld).style.top = '50%';
+	cardCounterOld = cardCounter;
+	cardCounter = cardCounter + 1;
 }
