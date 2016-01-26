d4a7d513e0455b772587a0de24ef7e243e1b4c6b
Douglas Cesar Pio do Carmo <doerdeiro@gmail.com>
2016-01-26 08:49:36 -0200

    Mudança de nome de variaveis no arquivo js

--- cards.js
+++ cards.js
@@ -2,10 +2,10 @@ var cardCounter = 0;
 var cardCounterOld = 0
 window.onload = function(){ 
   document.getElementById('card-creator').onclick = function() {
-    var iDiv = document.createElement('div');
-		iDiv.className = 'card';
-		iDiv.id = 'card-' + cardCounter;
-		document.getElementsByClassName('card-wrapper')[0].appendChild(iDiv);
+    var divCard = document.createElement('div');
+		divCard.className = 'card';
+		divCard.id = 'card-' + cardCounter;
+		document.getElementsByClassName('card-wrapper')[0].appendChild(divCard);
 		document.getElementById('card-' + cardCounterOld).style.left = '50%';
 		document.getElementById('card-' + cardCounterOld).style.top = '50%';
 		cardCounterOld = cardCounter;
