d8874ff3c75330aad30dfe89c7d35b645fb88980
Douglas Cesar Pio do Carmo <doerdeiro@gmail.com>
2016-01-26 08:03:10 -0200

    Implementacao do click em uma div e crie outra div (card).

--- cards.js
+++ cards.js
@@ -1,18 +1,14 @@
 var cardCounter = 0;
 var cardCounterOld = 0
-function cardCreator() { 
-	var iDiv = document.createElement('div');
-	iDiv.className = 'card';
-	iDiv.id = 'card-' + cardCounter;
-	document.getElementsByClassName('card-wrapper')[0].appendChild(iDiv);
-	document.getElementById('card-' + cardCounterOld).style.left = '50%';
-	document.getElementById('card-' + cardCounterOld).style.top = '50%';
-	cardCounterOld = cardCounter;
-	cardCounter = cardCounter + 1;
-}
-
 window.onload = function(){ 
   document.getElementById('card-creator').onclick = function() {
-    console.log('Oi');
+    var iDiv = document.createElement('div');
+		iDiv.className = 'card';
+		iDiv.id = 'card-' + cardCounter;
+		document.getElementsByClassName('card-wrapper')[0].appendChild(iDiv);
+		document.getElementById('card-' + cardCounterOld).style.left = '50%';
+		document.getElementById('card-' + cardCounterOld).style.top = '50%';
+		cardCounterOld = cardCounter;
+		cardCounter = cardCounter + 1;
 	};
 };
