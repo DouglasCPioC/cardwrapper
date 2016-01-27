3d513adeaf634f6d78a963817a2e6d7137ab39e7
Douglas Cesar Pio do Carmo <doerdeiro@gmail.com>
2016-01-26 22:39:06 -0200

    Implementacao da matematica para organizar os cards.
    - Matematica para organizar os cards de forma igualmente espacadas na
      medida que for adicionando novas divs (cards).
    - A matematica de organizacao ainda tem bugs para corrigir.

--- cards.js
+++ cards.js
@@ -1,13 +1,26 @@
 var cardCounter = 0;
-var cardCounterOld = 0
+var cardCounterOld = 0;
 window.onload = function(){ 
   document.getElementById('card-creator').onclick = function() {
     var divCard = document.createElement('div');
 		divCard.className = 'card';
 		divCard.id = 'card-' + cardCounter;
 		document.getElementsByClassName('card-wrapper')[0].appendChild(divCard);
-		document.getElementById('card-' + cardCounterOld).style.left = '50%';
-		document.getElementById('card-' + cardCounterOld).style.top = '50%';
+		var factor;
+		for(i = 0; i <= cardCounter; i++){
+			factor = ((150 / (cardCounter + 1)) / 300) * 100;
+			var leftValue;
+				if(i < (cardCounter + 1) / 2 && cardCounter != 0) {
+					leftValue = 50 - factor * (i % (cardCounter) / 2);
+					document.getElementById('card-' + i).style.top = '50%';	
+					document.getElementById('card-' + i).style.left = leftValue + '%';
+				}
+				else {
+					leftValue = 50 + factor * (i % (cardCounter) / 2);
+					document.getElementById('card-' + i).style.top = '50%';	
+					document.getElementById('card-' + i).style.left = leftValue + '%';
+				}
+		}
 		cardCounterOld = cardCounter;
 		cardCounter = cardCounter + 1;
 	};
