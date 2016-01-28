6e8b5179a6cc2699c2142b2655605bbbf73f7f66
Douglas Cesar Pio do Carmo <doerdeiro@gmail.com>
2016-01-28 16:31:54 -0200

    Correcao do bug de criacao do primeiro card fora da div.
    - Quando adicionava o primeiro card ele aparecia fora da div "card--wrapper".

--- cards.js
+++ cards.js
@@ -1,5 +1,4 @@
 var cardCounter = 0;
-var cardCounterOld = 0;
 window.onload = function(){ 
   document.getElementById('card-creator').onclick = function() {
     var divCard = document.createElement('div');
@@ -7,21 +6,20 @@ window.onload = function(){
 		divCard.id = 'card-' + cardCounter;
 		document.getElementsByClassName('card-wrapper')[0].appendChild(divCard);
 		var factor;
+		var leftValue;
 		for(i = 0; i <= cardCounter; i++){
 			factor = ((150 / (cardCounter + 1)) / 300) * 100;
-			var leftValue;
-				if(i < (cardCounter + 1) / 2 && cardCounter != 0) {
-					leftValue = 50 - factor * (i % (cardCounter) / 2);
-					document.getElementById('card-' + i).style.top = '50%';	
-					document.getElementById('card-' + i).style.left = leftValue + '%';
-				}
-				else {
-					leftValue = 50 + factor * (i % (cardCounter) / 2);
-					document.getElementById('card-' + i).style.top = '50%';	
-					document.getElementById('card-' + i).style.left = leftValue + '%';
-				}
+			if(i < (cardCounter + 1) / 2 && cardCounter != 0) {
+				leftValue = 50 - factor * ( (i % (cardCounter == 0 ? 1 : cardCounter) ) / 2);
+				document.getElementById('card-' + i).style.top = '50%';	
+				document.getElementById('card-' + i).style.left = leftValue + '%';
+			}
+			else {
+				leftValue = 50 + factor * ( (i % (cardCounter == 0 ? 1 : cardCounter) ) / 2);
+				document.getElementById('card-' + i).style.top = '50%';	
+				document.getElementById('card-' + i).style.left = leftValue + '%';
+			}
 		}
-		cardCounterOld = cardCounter;
 		cardCounter = cardCounter + 1;
 	};
 };
