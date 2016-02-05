7221f53827e5fda5762fa53deb5ae5afdcab4dc0
Douglas Cesar Pio do Carmo <doerdeiro@gmail.com>
2016-02-05 05:27:39 -0200

    Refatorando Javascript, estrutura de pastas criada e inclusao de jquery
    - Refatoramento parcial do js.
    - Inclusao parcial do css dentro do js.
    - Criacao de estrutura de pastas.
    - Inclusao de jquery no projeto.

--- js/cards.js
+++ js/cards.js
@@ -14,6 +14,7 @@ function setHeight(value) {
 	this.height = value;
 }
 
+// Work correctly
 function Card() {
 	this.name = 'card';
 	this.width = "100px";
@@ -25,14 +26,14 @@ function Card() {
 }
 
 // Work correctly
-function create() {
+function draw() {
 	this.div = document.createElement('div');
 	this.div.className = 'card-wrapper';
 	this.div.id = 'card-wrapper';
 	document.getElementsByTagName('body')[0].appendChild(this.div);
 	document.getElementById(this.div.id).style.width = this.getWidth();
 	document.getElementById(this.div.id).style.height = this.getHeight();
-	document.getElementById(this.div.id).style.background = 'blue';
+	document.getElementById(this.div.id).style.background = '#202020';
 	document.getElementById(this.div.id).style.top = '50%';
 	document.getElementById(this.div.id).style.left = '50%';
 	document.getElementById(this.div.id).style.position = 'fixed';
@@ -50,19 +51,25 @@ function Wrapper() {
 	this.setWidth = setWidth;
 	this.getHeight = getHeight;
 	this.setHeight = setHeight;
-	this.create = create;
+	this.draw = draw;
+	this.draw()
 }
-//wrapperObject = new Wrapper();
-//wrapperObject.create();
 
+// Work correctly
 var cardCounter = 0;
 function addCard(cardObject) {
 	this.divCard = document.createElement('div');
 	this.divCard.className = cardObject.name;
 	this.divCard.id = cardObject.name + '-' + cardCounter;
-	document.getElementsByClassName('card-wrapper')[0].appendChild(divCard);
+	document.getElementsByClassName('card-wrapper')[0].appendChild(this.divCard);
 	document.getElementById(cardObject.name + '-' + cardCounter).style.width = cardObject.getWidth();
 	document.getElementById(cardObject.name + '-' + cardCounter).style.height = cardObject.getHeight();
+	document.getElementById(cardObject.name + '-' + cardCounter).style.background = 'red';
+	document.getElementById(cardObject.name + '-' + cardCounter).style.position = 'fixed';
+	document.getElementById(cardObject.name + '-' + cardCounter).style.top = '50%';
+	document.getElementById(cardObject.name + '-' + cardCounter).style.left = '50%';
+	document.getElementById(cardObject.name + '-' + cardCounter).style.marginTop = '-50px';
+	document.getElementById(cardObject.name + '-' + cardCounter).style.marginLeft = '-50px';
 	cardCounter = cardCounter + 1;
 }
 
@@ -70,4 +77,20 @@ function Engine(context, options) {
 	this.wrapperObj = context;
 	this.parametersObj = options;
 	this.addCard = addCard;
+}
+
+// For test
+window.onload = function() {
+	wrapperObject = new Wrapper();
+	engineScreen = new Engine(wrapperObject, {});
+	card = new Card();
+	engineScreen.addCard(card);
+	
+	$('.card').hover(function() {
+		$(this)[0].style.transitionDuration = '2s';
+		$(this)[0].style.transform = 'rotateY(45deg)';
+	}, function() {
+		$(this)[0].style.transitionDuration = '2s';
+		$(this)[0].style.transform = 'rotateY(0deg)';
+	});
 }
